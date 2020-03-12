c6aad237c959c3344281ac895c8a575034a3645c
Aydar Shakirov <shaaydar@yandex.ru>
2020-03-12 11:24:13 +0500

    Добавьте файлы проекта.

---
+++ ILoveBasicLaba2/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace ILoveBasicLaba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.tabPage8 = new System.Windows.Forms.TabPage();
+            this.tabPage9 = new System.Windows.Forms.TabPage();
+            this.tabPage10 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Controls.Add(this.tabPage8);
+            this.tabControl1.Controls.Add(this.tabPage9);
+            this.tabControl1.Controls.Add(this.tabPage10);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(867, 605);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(859, 572);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Пример 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            this.tabPage1.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage1_Paint);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(859, 572);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Пример 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage2_Paint);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(859, 572);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Пример 3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.button2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 29);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(859, 572);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Пример 4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 29);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(859, 572);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Пример 5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            this.tabPage5.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage5_Paint);
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(4, 29);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(859, 572);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Пример 6";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            this.tabPage6.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage6_Paint);
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Location = new System.Drawing.Point(4, 29);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage7.Size = new System.Drawing.Size(859, 572);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "Пример 7";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            this.tabPage7.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage7_Paint);
+            // 
+            // tabPage8
+            // 
+            this.tabPage8.Location = new System.Drawing.Point(4, 29);
+            this.tabPage8.Name = "tabPage8";
+            this.tabPage8.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage8.Size = new System.Drawing.Size(859, 572);
+            this.tabPage8.TabIndex = 7;
+            this.tabPage8.Text = "Пример 8";
+            this.tabPage8.UseVisualStyleBackColor = true;
+            // 
+            // tabPage9
+            // 
+            this.tabPage9.Location = new System.Drawing.Point(4, 29);
+            this.tabPage9.Name = "tabPage9";
+            this.tabPage9.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage9.Size = new System.Drawing.Size(859, 572);
+            this.tabPage9.TabIndex = 8;
+            this.tabPage9.Text = "Пример 9";
+            this.tabPage9.UseVisualStyleBackColor = true;
+            // 
+            // tabPage10
+            // 
+            this.tabPage10.Location = new System.Drawing.Point(4, 29);
+            this.tabPage10.Name = "tabPage10";
+            this.tabPage10.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage10.Size = new System.Drawing.Size(859, 572);
+            this.tabPage10.TabIndex = 9;
+            this.tabPage10.Text = "Пример 10";
+            this.tabPage10.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(8, 525);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Показать кривую Безье";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(8, 525);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Показать кривую Безье";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 605);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.TabPage tabPage8;
+        private System.Windows.Forms.TabPage tabPage9;
+        private System.Windows.Forms.TabPage tabPage10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ILoveBasicLaba2/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ILoveBasicLaba2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Paint(object sender, PaintEventArgs e)
+        {
+            //    Dim G As Graphics = pe.Graphics
+            //' Далее можно вставить какой-нибудь код для рисования
+            //Dim Point1 As New Point(100, 100)
+            //Dim Point2 As New Point(200, 200)
+            //Dim MyPen As New Pen(Color.Red, 3)
+            //G.DrawLine(MyPen, Point1, Point2)
+            e.Graphics.DrawLine(new Pen(Color.Red, 3), new Point(100, 100), new Point(200, 200));
+        }
+
+        private void tabPage2_Paint(object sender, PaintEventArgs e)
+        {
+            //Dim g As Graphics = pe.Graphics
+            //Dim MyBox As New Rectangle(5, 10, 150, 200)
+            //Dim MyPen As New Pen(Color.Blue, 2)
+            //g.DrawEllipse(MyPen, MyBox)
+            e.Graphics.DrawEllipse(new Pen(Color.Blue, 2), new Rectangle(5, 10, 150, 200));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Dim s1 As New Point(10, 100) ' Начальная точка
+            //Dim e1 As New Point(100, 10) ' Первая контрольная точка
+            //Dim e2 As New Point(150, 150) ' Вторая контрольная точка
+            //Dim s2 As New Point(200, 100) ' Конечная точка
+            //Dim G As Graphics
+            //G = MyPictureBox.CreateGraphics
+            //G.Clear(Color.White)
+            //Dim Mypen As New Pen(Color.Black, 3)
+            //G.DrawBezier(Mypen, s1, e1, e2, s2)
+
+            var g = tabPage3.CreateGraphics();
+            g.Clear(Color.White);
+            g.DrawBezier(new Pen(Color.Black, 3), 
+                new Point(10, 100), new Point(100, 10), 
+                new Point(150, 150), new Point(200, 100));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Dim MyPoints As Point() = {
+            //New Point(10, 100), _
+            //New Point(75, 10), New Point(80, 50), _
+            //New Point(100, 150), New Point(125, 80), _
+            //New Point(175, 200), New Point(200, 80)}
+            //Dim G As Graphics
+            //G = MyPictureBox.CreateGraphics
+            //G.Clear(Color.White)
+            //Dim Mypen As New Pen(Color.Red, 3)
+            //G.DrawBeziers(Mypen, MyPoints)
+
+            var g = tabPage4.CreateGraphics();
+            g.Clear(Color.White);
+
+            g.DrawBeziers(
+                new Pen(Color.Black, 3), 
+                new Point[] { 
+                    new Point(10, 100), new Point(75, 10),
+                    new Point(80, 50), new Point(10, 150),
+                    new Point(125, 80), new Point(175, 200),
+                    new Point(200, 80) 
+                } 
+                );
+
+        }
+
+        private void tabPage5_Paint(object sender, PaintEventArgs e)
+        {
+            //' Создаем однотонную кисть
+            //Dim blueBrush As New SolidBrush(Color.Blue)
+            //' указываем расположение и размеры эллипса
+            //Dim x As Single = 0.0F
+            //Dim y As Single = 0.0F
+            //Dim width As Single = 150.0F
+            //Dim height As Single = 150.0F
+            //' Указываем начальный и конечный углы
+            //Dim startAngle As Single = 0.0F
+            //Dim endAngle As Single = 120.0F
+            //' Заливаем сегмент
+            //pe.Graphics.FillPie(blueBrush, x, y, width, height, _
+            //startAngle, endAngle)
+
+            float x = .0F;
+            float y = .0F;
+            float width = 150.0F;
+            float height = 150.0F;
+
+            float startAngle = .0F;
+            float endAngle = 120.0F;
+
+            e.Graphics.FillPie(
+                new SolidBrush(Color.Blue), x, y, width, 
+                height, startAngle, endAngle);
+        }
+
+        private void tabPage6_Paint(object sender, PaintEventArgs e)
+        {
+            //Dim G As Graphics
+            //G = MyPictureBox.CreateGraphics
+            //Dim brush As New SolidBrush(Color.Honeydew)
+            //Dim Angles() As Single = { 0, 130, 205, 290, 360 }
+            //Dim Colors() As Color = {Color.LightGoldenrodYellow, _
+            //Color.PaleTurquoise, Color.RoyalBlue, Color.Purple}
+            //G.Clear(Color.Ivory)
+            //Dim rect As New Rectangle(10, 50, 250, 150)
+            //Dim angle As Integer
+            //For angle = 1 To Angles.Length - 1
+            //    brush.Color = Colors(angle - 1)
+            //    G.FillPie(brush, rect, Angles(angle - 1), _
+            //    Angles(angle) - Angles(angle - 1))
+            //Next
+            //G.DrawEllipse(Pens.Black, rect)
+
+            var rect = new Rectangle(10, 50, 250, 150);
+            float[] angles = new float[] { 0, 130, 205, 290, 360 };
+            var colors = new Color[] { 
+                Color.LightGoldenrodYellow, Color.PaleTurquoise, 
+                Color.RoyalBlue, Color.Purple 
+            };
+            for (int i = 0; i < angles.Length - 1; i++)
+            {
+                e.Graphics.FillPie(new SolidBrush(colors[i]),
+                    rect, angles[i],
+                    angles[i + 1] - angles[i]
+                );
+            }
+
+            e.Graphics.DrawEllipse(Pens.Black, rect);
+        }
+
+        private void tabPage7_Paint(object sender, PaintEventArgs e)
+        {
+            //    'Определяем штриховую кисть
+            //Dim hBrush As New HatchBrush(HatchStyle.DarkUpwardDiagonal, _
+            //Color.DarkGoldenrod, Color.Crimson)
+            //' Создаем точки, определяющие полигон
+            //Dim point1 As New PointF(0.0F, 0.0F)
+            //Dim point2 As New PointF(100.0F, 25.0F)
+            //Dim point3 As New PointF(200.0F, 5.0F)
+            //Dim point4 As New PointF(250.0F, 50.0F)
+            //Dim point5 As New PointF(300.0F, 100.0F)
+            //Dim point6 As New PointF(350.0F, 200.0F)
+            //Dim point7 As New PointF(200.0F, 200.0F)
+            //Dim point8 As New PointF(130.0F, 230.0F)
+            //Dim curvePoints As PointF() = {
+            //        point1, point2, point3, point4, _
+            //point5, point6, point7, point8}
+            //    ' Определяем режим заливки
+            //Dim newFillMode As FillMode = FillMode.Winding
+            //' Заливаем полигон
+            //pe.Graphics.FillPolygon(hBrush, curvePoints, newFillMode)
+
+            var points = new PointF[]
+            {
+                new PointF(0.0F, 0.0F),
+                new PointF(100.0F, 25.0F),
+                new PointF(200.0F, 5.0F),
+                new PointF(250.0F, 50.0F),
+                new PointF(300.0F, 100.0F),
+                new PointF(350.0F, 200.0F),
+                new PointF(200.0F, 200.0F),
+                new PointF(130.0F, 230.0F)
+            };
+            e.Graphics.FillPolygon(
+                new HatchBrush(
+                    HatchStyle.DarkUpwardDiagonal,
+                    Color.DarkGoldenrod,
+                    Color.Crimson
+                ),
+                points,
+                FillMode.Winding
+            );
+        }
+    }
+}            
